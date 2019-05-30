55d7329624adc4edac3b352fbf9a5431404f9fff
ez4navi <Ilya.Lisovski@yandex.ru>
2019-05-30 16:21:00 +0300

    add dividing lines and save to local storage

--- js/script.js
+++ js/script.js
@@ -1,48 +1,93 @@
 const section = document.querySelector("body > main > section"),
   input = document.querySelector("body > main > section > input"),
-  toDoListVar = [], createToDoList = document.createElement('ul');
-  let newToDo = '', toDoList, idCounter = 0;
-
+  createToDoList = document.createElement('ul');
+  let newToDo = '', toDoList = document.querySelector("#toDoList"), counter = 0, toDoListArr = [];
+  var $valueClass = $('.value:last');
 
 function clearInput() {
     input.value = '';
 }
 
-function insertToDo() {
+function updateToDoToLocalStorage() {
+    let sItem = JSON.stringify(toDoListArr);
+    localStorage.setItem('memory',sItem);
+}
+
+function insertToDo(item) {
     $('ul').append($('<li class="list-item">'));
     $('li:last').append('<div class="todo">');
-    $('div:last').append('<div class="checkbox-label">' + '<label class="value">' + input.value +
-      '</label></div>' + '<button class="cross"></button>');
-    $('.value:last').before(function () {
+    if (item) {
+        $('div:last').append('<div class="checkbox-label">' + '<label class="value">' +
+          item.split(':')[0] + '</label></div>' + '<button class="cross"></button>');
+    } else {
+        $('div:last').append('<div class="checkbox-label">' + '<label class="value">' + input.value +
+          '</label></div>' + '<button class="cross"></button>');
+    }
+
+    let $valueLast = $('.value:last');
+
+    $valueLast.before(function () {
         return $('<div class="checkbox"></div>').click(function () {
+            let i = $(this).closest('li').index();
+            if (toDoListArr[i].split(':')[1] === 'false') {
+                toDoListArr[i] = toDoListArr[i].replace('false','true');
+                updateToDoToLocalStorage();
+            } else {
+                toDoListArr[i] = toDoListArr[i].replace('true','false');
+                updateToDoToLocalStorage();
+            }
             $(this).toggleClass('checked');
             $(this).next().toggleClass('checked');
         });
     });
+    if (item) {
+        if (item.split(':')[1] === 'true') {
+            $('.checkbox:last').toggleClass('checked');
+            $valueLast.toggleClass('checked');
+        }
+    }
     $('.list-item').hover(function () {
        $(this).find('.cross').css('visibility','visible');
     }, function () {
        $(this).find('.cross').css('visibility','hidden');
     });
-    $('.cross').click(function () {
+    $('.cross:last').click(function () {
+        toDoListArr.splice($(this).closest('li').index(),1);
         $(this).closest('.list-item').remove();
-    })
+        updateToDoToLocalStorage();
+    });
 }
 
 section.addEventListener('keypress', function (e) {
     if (e.which === 13 && input.value.length > 0 && input.value.trim()) {
-        toDoListVar.push(input.value);
+        toDoListArr.push(input.value+':false');
         if(!$('#mainSection').children('#toDoList').length > 0) {
             section.appendChild(createToDoList);
             createToDoList.setAttribute('id','toDoList');
-            toDoList = document.querySelector("#toDoList");
             insertToDo();
             clearInput();
+            updateToDoToLocalStorage();
         } else {
             insertToDo();
             clearInput();
+            updateToDoToLocalStorage();
         }
     }
 });
 
+$(document).ready(function(){
+    if (localStorage.getItem('memory')) {
+        toDoListArr = JSON.parse(localStorage.getItem('memory'));
+        loadFromLocalStorage();
+    }
+});
+
+
+function loadFromLocalStorage() {
+    section.appendChild(createToDoList);
+    createToDoList.setAttribute('id','toDoList');
+    toDoListArr.forEach((element)=> {
+        insertToDo(element);
+    })
+}
 
