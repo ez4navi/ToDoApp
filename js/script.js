c362bccac6ff004b1a05ae5ba6374c8fe15dd32e
ez4navi <Ilya.Lisovski@yandex.ru>
2019-06-05 12:22:24 +0300

    feat: add toggle all button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,44 @@
-const section = document.querySelector("body > main > section"),
-  input = document.querySelector("body > main > section > input"),
-  createToDoList = document.createElement('ul');
-  let newToDo = '', toDoList = document.querySelector("#toDoList"), counter = 0, toDoListArr = [];
-  var $valueClass = $('.value:last');
+const section = document.querySelector("#mainSection"),
+  input = document.querySelector("#new-todo"),
+  createToDoList = document.createElement('ul'),
+  $checkAllMark = $('#mainSection > label'),
+  $toggleAll = $('#toggle-all');
+  let newToDo = '', toDoList = document.querySelector("#toDoList"), counter, toDoListArr = [];
 
 function clearInput() {
     input.value = '';
 }
 
+function showCheckAllMark() {
+    $checkAllMark.css('opacity','1');
+}
+
+function hideCheckAllMark() {
+    $checkAllMark.css('opacity','0');
+}
+
 function updateToDoToLocalStorage() {
     let sItem = JSON.stringify(toDoListArr);
     localStorage.setItem('memory',sItem);
+}
+
+function checkAllCheckboxes() {
+    if (toDoListArr.every(item => item.split(':')[1] === 'true')) {
+        $toggleAll.prop('checked', true);
+    }
+}
+
+function loadFromLocalStorage() {
+    section.appendChild(createToDoList);
+    createToDoList.setAttribute('id','toDoList');
+    toDoListArr.forEach((element)=> {
+        insertToDo(element);
+    });
+    if (localStorage.memory.length > 2) {
+        showCheckAllMark();
+        checkAllCheckboxes();
+    }
+
 }
 
 function insertToDo(item) {
@@ -38,6 +66,12 @@ function insertToDo(item) {
             }
             $(this).toggleClass('checked');
             $(this).next().toggleClass('checked');
+
+            if (toDoListArr.every(item => item.split(':')[1] === 'true')) {
+                $toggleAll.prop('checked', true);
+            } else {
+                $toggleAll.prop('checked', false);
+            }
         });
     });
     if (item) {
@@ -55,12 +89,20 @@ function insertToDo(item) {
         toDoListArr.splice($(this).closest('li').index(),1);
         $(this).closest('.list-item').remove();
         updateToDoToLocalStorage();
+        if (localStorage.memory.length === 2) {
+            hideCheckAllMark();
+        }
+        checkAllCheckboxes();
+        if (toDoListArr.length === 0) {
+            $toggleAll.prop('checked', false);
+        }
     });
 }
 
 section.addEventListener('keypress', function (e) {
     if (e.which === 13 && input.value.length > 0 && input.value.trim()) {
         toDoListArr.push(input.value+':false');
+        showCheckAllMark();
         if(!$('#mainSection').children('#toDoList').length > 0) {
             section.appendChild(createToDoList);
             createToDoList.setAttribute('id','toDoList');
@@ -82,12 +124,24 @@ $(document).ready(function(){
     }
 });
 
+$toggleAll.change(function () {
+    const $checkbox = $('.checkbox');
+    if(this.checked) {
+        toDoListArr.forEach((item,idx,array) => {
+           array[idx] = item.split(':')[0]+':true';
+        });
+        $checkbox.addClass('checked');
+        $checkbox.next().addClass('checked');
+    } else {
+        toDoListArr.forEach((item,idx,array) => {
+            array[idx] = item.split(':')[0]+':false';
+        });
+        $checkbox.removeClass('checked');
+        $checkbox.next().removeClass('checked');
+    }
 
-function loadFromLocalStorage() {
-    section.appendChild(createToDoList);
-    createToDoList.setAttribute('id','toDoList');
-    toDoListArr.forEach((element)=> {
-        insertToDo(element);
-    })
-}
+    updateToDoToLocalStorage();
+});
+
+
 
